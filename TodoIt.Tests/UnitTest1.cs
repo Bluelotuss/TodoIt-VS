4c2536b16e24433aa2fee6c4e5967cb0436c03ff
sdg <sg>
2020-10-01 19:13:48 +0200

    Person tests

---
+++ TodoIt.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System;
+using Xunit;
+
+namespace TodoIt.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void PersonConstructor()
+        {
+            //Arrange
+            int personId = 202020;
+            string firstName = "John";
+            string lastName = "Doe";
+
+            //Act
+            Person result = new Person(personId, firstName, lastName);
+
+            //Assert
+            Assert.Contains(personId.ToString(), result.PersonInfomation());
+            Assert.Contains(firstName, result.PersonInfomation());
+            Assert.Contains(lastName, result.PersonInfomation());
+
+        }
+
+        [Theory]// replace Fact to test with many alternate values that are bad.
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData(" ", " ")]
+        //[InlineData(" will fail ")]
+        public void BadColorsConstructorCar(string firstName, string lastName)
+        {
+            //Arrange
+            int personId = 20312420;
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => new Person(personId, firstName, lastName));
+
+        }
+
+    }
+}
---
+++ TodoIt/Model/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+namespace TodoIt
+{
+    public class Person
+    {
+        //fields
+        public int personId { get; set; }
+        string firstName;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("First name cannot be null or empty");
+                }
+                else
+                {
+                    firstName = value;
+                }
+            }
+        }
+        string lastName;
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Last name cannot be null or empty");
+                }
+                else
+                {
+                    lastName = value;
+                }
+            }
+        }
+
+        //constructor
+        public Person(int personId, string firstName, string lastName)
+        {
+            this.personId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string PersonInfomation()
+        {
+            return $"Person id:{personId} | First name: {firstName}\nLast name: {lastName}\n\n";
+        }
+
+    }
+}

2333728a0bfe150382f3505669395ecace85b2f3
sdg <sg>
2020-10-02 10:52:13 +0200

    Todosequencer tests

---
+++ TodoIt/Data/TodoSequencer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TodoIt
+{
+    public class TodoSequencer
+    {
+        static int todoId;
+
+        public static int NextTodoId()
+        {
+            return ++todoId;
+        }
+
+        public static void Reset()
+        {
+            todoId = 0;
+        }
+    }
+}

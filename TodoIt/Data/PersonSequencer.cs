ff4477a400d8effcb363cbf28ca301f8c8efedd8
sdg <sg>
2020-10-04 19:12:34 +0200

    Personsequencer commited and pushed

---
+++ TodoIt/Data/PersonSequencer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TodoIt
+{
+    public class PersonSequencer
+    { 
+            static int personId;
+
+            public static int NextPersonId()
+            {
+                return ++personId;
+            }
+
+            public static void Reset()
+            {
+                personId = 0;
+            }
+        
+    }
+}
